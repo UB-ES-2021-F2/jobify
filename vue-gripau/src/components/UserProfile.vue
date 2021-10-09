c94fb2f11511681f19e73c6b3a22001fdc294eb9
Arnau Quindós <aquindsa10@alumnes.ub.edu>
2021-10-09 17:26:37 +0200

    Set up vue and bootstrap and Router for future dev

---
+++ vue-gripau/src/components/UserProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1> {{ message }} </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      message: 'User profile'
+    }
+  }
+}
+
+</script>

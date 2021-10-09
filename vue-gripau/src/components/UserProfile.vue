9e15393fa04513f1d983b3f9b610e18c4cd43a41
Arnau Quindós <aquindsa10@alumnes.ub.edu>
2021-10-09 21:49:52 +0200

    Fonts added to project

--- vue-gripau/src/components/UserProfile.vue
+++ vue-gripau/src/components/UserProfile.vue
@@ -1,6 +1,48 @@
 <template>
   <div id="app">
-    <h1> {{ message }} </h1>
+    <!--Navbar -->
+    <nav class="mb-1 navbar navbar-expand-lg navbar-light bg-white py-4">
+      <a class="navbar-brand">
+        <img style="max-width: 150px" :src="require('../assets/logo.svg')">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+              aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent-333" style="font-size:18px;
+       font-family:'Work Sans SemiBold'">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="onHome()">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Job postings</a>
+          </li>
+          <li class="nav-item" @click="onAboutUs()">
+            <a class="nav-link" href="#">About us</a>
+          </li>
+        </ul>
+        <ul v-if="!logged" class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="onLogIn()">Log in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign up</a>
+          </li>
+        </ul>
+        <ul v-if="logged" class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">User</a>
+          </li>
+          <button class="btn btn-outline-danger" @click="onLogOut()"> Log Out </button>
+        </ul>
+      </div>
+    </nav>
+    <!--/.Navbar -->
+
+    <h1 style="font-family: 'Vollkorn"> {{ name }} </h1>
   </div>
 </template>
 
@@ -9,7 +51,22 @@
 export default {
   data () {
     return {
-      message: 'User profile'
+      name: 'Name Surname',
+      logged: true
+    }
+  },
+  methods: {
+    onHome () {
+      this.$router.replace({ path: '/' })
+    },
+    onLogIn () {
+      this.logged = true
+    },
+    onLogOut () {
+      this.logged = false
+    },
+    onAboutUs () {
+      this.$router.replace({ path: '/about_us' })
     }
   }
 }

f9a40070371ba6195117725c6f1f2d1a7db3d035
Sergi Ger <sergiger00@gmail.com>
2021-10-13 00:38:44 +0200

    Style changes

--- vue-gripau/src/components/Home.vue
+++ vue-gripau/src/components/Home.vue
@@ -48,28 +48,29 @@
     <b-modal ref="editShowModal"
              id="event-modal"
              title="Become a member"
-             hide-footer>
-      <b-form @submit="onSubmit" @reset="onReset">
+             hide-footer
+    >
+      <b-form style="font-family:'Work Sans'" @submit="onSubmit" @reset="onReset">
         <label style="color: #5a6268">All fields are needed.</label>
 
-          <b-form-group id="input-group-0" label="First name:" label-for="input-0">
-            <b-form-input v-model="register.fName" placeholder="" type="text" required></b-form-input>
-          </b-form-group>
+        <b-form-group id="input-group-0" label="First name:" label-for="input-0">
+          <b-form-input v-model="register.fName" placeholder="" type="text" required></b-form-input>
+        </b-form-group>
 
-          <b-form-group id="input-group-1" label="Last name:" label-for="input-1">
-            <b-form-input v-model="register.lName" placeholder="" type="text" required></b-form-input>
-          </b-form-group>
+        <b-form-group id="input-group-1" label="Last name:" label-for="input-1">
+          <b-form-input v-model="register.lName" placeholder="" type="text" required></b-form-input>
+        </b-form-group>
 
-          <b-form-group id="input-group-2" label="Email:" label-for="input-2">
-            <b-form-input v-model="register.email" placeholder="" type="email" required></b-form-input>
-          </b-form-group>
+        <b-form-group id="input-group-2" label="Email:" label-for="input-2">
+          <b-form-input v-model="register.email" placeholder="" type="email" required></b-form-input>
+        </b-form-group>
 
-          <b-form-group id="input-group-3" label="Password:" label-for="input-3">
-            <b-form-input v-model="register.password" placeholder="" type="password" required></b-form-input>
-            <b-form-text id="password-help-block">
-              Your password should be 8-20 characters long.
-            </b-form-text>
-          </b-form-group>
+        <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+          <b-form-input v-model="register.password" placeholder="" type="password" required></b-form-input>
+          <b-form-text id="password-help-block">
+            Your password should be 8-20 characters long.
+          </b-form-text>
+        </b-form-group>
 
         <b-form-checkbox id="checkbox-1" :state="termsAndCState" v-model="register.rTandC" name="checkbox-1" required>
           I have read and accept the terms and conditions and privacy policy.

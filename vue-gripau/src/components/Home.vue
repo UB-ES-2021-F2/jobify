27e4c985d06ce624268fd6fc672afed6d1e05f3d
Sergi Ger <sergiger00@gmail.com>
2021-10-12 20:25:16 +0200

    register v2

--- vue-gripau/src/components/Home.vue
+++ vue-gripau/src/components/Home.vue
@@ -2,57 +2,51 @@
   <div id="app">
     <h1> {{ message }} </h1>
     <button class="btn btn-primary btn-lg" @click="onUserProfile()"> User Profile </button>
-    <button class="btn btn-lg" v-b-modal.event-modal1>Register</button>
+    <button class="btn btn-lg" v-b-modal.event-modal>Register</button>
     <b-modal ref="editShowModal"
-             id="event-modal1"
+             id="event-modal"
              title="Become a member"
              hide-footer>
-      <b-form @reset="onReset">
+      <b-form @submit="onSubmit" @reset="onReset">
         <label style="color: #5a6268">All fields are needed.</label>
+
           <b-form-group id="input-group-0" label="First name:" label-for="input-0">
-            <b-form-input v-model="register.fName" placeholder="" input type="text"></b-form-input>
+            <b-form-input v-model="register.fName" placeholder="" type="text" required></b-form-input>
           </b-form-group>
+
           <b-form-group id="input-group-1" label="Last name:" label-for="input-1">
-            <b-form-input v-model="register.lName" placeholder="" input type="text"></b-form-input>
+            <b-form-input v-model="register.lName" placeholder="" type="text" required></b-form-input>
           </b-form-group>
-          <b-form-group @submit.stop.prevent id="input-group-2" label="Email:" label-for="input-2">
-            <b-form-input v-model="register.email" placeholder="" :state="validationEmail" input type="email"></b-form-input>
-            <b-form-invalid-feedback :state="validationPwd">
-              Your email must be 5-12 characters long.
-            </b-form-invalid-feedback>
-            <b-form-valid-feedback :state="validationPwd">
-              Looks Good.
-            </b-form-valid-feedback>
+
+          <b-form-group id="input-group-2" label="Email:" label-for="input-2">
+            <b-form-input v-model="register.email" placeholder="" type="email" required></b-form-input>
           </b-form-group>
+
           <b-form-group id="input-group-3" label="Password:" label-for="input-3">
-            <b-form-input v-model="register.password" placeholder="" input type="password"></b-form-input>
+            <b-form-input v-model="register.password" placeholder="" type="password" required></b-form-input>
             <b-form-text id="password-help-block">
-              Your password must be 8-20 characters long, contain letters and numbers, and must not
-              contain spaces, special characters, or emoji.
+              Your password should be 8-20 characters long.
             </b-form-text>
           </b-form-group>
-        <form class="was-validated">
-          <div class="custom-control custom-checkbox mb-3">
-            <input type="checkbox" class="custom-control-input" id="customControlValidation1" required>
-            <label class="custom-control-label" for="customControlValidation1">I have read and accept the terms and conditions and privacy policy.</label>
-          </div>
-        </form>
-        <div class="form-check">
-          <input class="custom-control-input" type="checkbox" value="" id="invalidCheck" required>
-          <label class="custom-control-label" for="invalidCheck">
-            I do not want to receive the Jungle Newsletter and tips to optimise my job search.
-          </label>
-        </div>
+
+        <b-form-checkbox id="checkbox-1" :state="termsAndCState" v-model="register.rTandC" name="checkbox-1" required>
+          I have read and accept the terms and conditions and privacy policy.
+        </b-form-checkbox>
+
+        <b-form-checkbox id="checkbox-2" v-model="register.newsletter" name="checkbox-2">
+          I do not want to receive the Jungle Newsletter and tips to optimise my job search.
+        </b-form-checkbox>
+
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <button class="btn btn-warning justify-content-md-end" @click="onSubmitUpdate">Submit</button>
+          <button class="btn btn-warning justify-content-md-end">Submit</button>
         </div>
+
       </b-form>
     </b-modal>
   </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
@@ -61,7 +55,9 @@ export default {
         fName: '',
         lName: '',
         email: '',
-        password: ''
+        password: '',
+        rTandC: false,
+        newsletter: false
       }
     }
   },
@@ -69,9 +65,8 @@ export default {
     onUserProfile () {
       this.$router.replace({ path: '/user' })
     },
-    onSubmitUpdate (evt) {
-      evt.preventDefault()
-      this.$refs.editShowModal.hide()
+    onSubmit () {
+      alert('Form submitted!')
     },
     initForm () {
       this.register.fName = ''
@@ -86,19 +81,11 @@ export default {
       this.$nextTick(() => {
         this.show = true
       })
-    },
-    onResetUpdate (evt) {
-      evt.preventDefault()
-      this.initForm()
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
     }
   },
   computed: {
-    validationEmail () {
-      return this.register.email.length > 4 && this.register.email.length < 13
+    termsAndCState () {
+      return this.register.rTandC
     }
   }
 }

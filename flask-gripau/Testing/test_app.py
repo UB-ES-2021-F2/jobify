46daf0a24a588069519aa4dc35025938880750c1
Sergi Bech Sala <sergi.bechsala@bsc.es>
2021-10-16 19:58:41 +0200

    Test delete educations added

--- flask-gripau/Testing/test_app.py
+++ flask-gripau/Testing/test_app.py
@@ -1,4 +1,5 @@
 from models.job_seeker import JobSeekersModel
+from models.education import EducationsModel
 from Testing import BaseTestCase
 from db import db
 
@@ -38,6 +39,14 @@ class TestJobSeeker(BaseTestCase):
         new_job_seeker.delete_from_db(db)
         result = db.session.query(JobSeekersModel).first()
         self.assertIsNone(result, 'Nothing in the database')
+
+    def test_delete_education(self):
+        new_job_seeker = JobSeekersModel('test', 'test@hotmail.com', 'hola, soc un test')
+        new_job_seeker.hash_password('test')
+        new_education = EducationsModel('Maths phd', 'UB', '09-2021', '10-2022', True)
+        new_job_seeker.educations.append(new_education)
+        new_job_seeker.delete_education(None)
+        assert new_job_seeker.educations == []
 
 
 if __name__ == '__main__':

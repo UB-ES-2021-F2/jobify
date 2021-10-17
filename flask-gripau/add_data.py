063d4e4616466519e6a0f14256baddb991ce8dc4
alendinez <55415649+alendinez@users.noreply.github.com>
2021-10-17 19:29:46 +0200

    added put method in companies endpoint

--- flask-gripau/add_data.py
+++ flask-gripau/add_data.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#import models here
+# import models here
+from models import EducationsModel, JobOfferModel
 from models.job_seeker import JobSeekersModel
 from models.company import CompanyModel
 
@@ -10,10 +13,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 
-new_job_seeker = JobSeekersModel('sergi','sergi@gmail.com', 'hola, soc estudiant')
+new_job_seeker = JobSeekersModel('sergi', 'sergi@gmail.com', 'hola, soc estudiant')
+new_job_seeker.hash_password('password')
+new_education = EducationsModel('Maths phd', 'UB', '09-2021', '10-2022', True)
+new_job_seeker.educations.append(new_education)
 db.session.add(new_job_seeker)
 
-new_company = CompanyModel('UB','ub@gmail.com', 'hola, som la UB')
+new_company = CompanyModel('UB', 'ub@gmail.com', 'hola, som la UB')
+new_job_offer = JobOfferModel('professor', 'professor de EDS', datetime.strptime('2021-07-04', "%Y-%m-%d"), 'Barcelona')
+new_company.hash_password('password')
+new_company.job_offers.append(new_job_offer)
 db.session.add(new_company)
 
 db.session.commit()

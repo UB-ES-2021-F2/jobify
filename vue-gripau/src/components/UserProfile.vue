dd8c5f30f1eb69e72ec756c766973fe7c383283c
Arnau Quindós <aquindsa10@alumnes.ub.edu>
2021-10-17 19:48:15 +0200

    Finished user profile template

--- vue-gripau/src/components/UserProfile.vue
+++ vue-gripau/src/components/UserProfile.vue
@@ -27,129 +27,125 @@
     </b-navbar>
     <!--/.Navbar -->
 
-    <h1 style="font-family: 'Vollkorn"> {{ name }} </h1>
+    <b-container>
 
-    <div class="m-5">
-      <div class="p-3 text-left">
-        <h3 style="font-family: 'Vollkorn'; display:inline-block"> Work experience </h3>
-        <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px" @click="onAddWork()">Add work experience</button>
-        <div class="card mb-3" style="max-width: 700px"  v-for="work in work_experience" :key="work.id">
-          <div class="card-header align-items-center">
-            <div class="row align-items-center">
-              <div class="col">
-                <h5 class="card-title">{{work.job_name}}</h5>
-              </div>
-              <div class="d-flex flex-row-reverse">
-                <button class="btn btn-sm btn-danger m-1" @click="deleteWork(work)">Delete</button>
-                <!-- <button class="btn btn-sm m-1">Edit</button> -->
-              </div>
+      <h2 style="font-family: 'Vollkorn"> {{ name }} </h2>
+
+      <div class="container-md-5 p-2 align-items-center">
+        <div class="bio-text">
+          {{bio}}
+        </div>
+
+        <div class="text-left p-2 pb-3" style="max-width: 50rem">
+          <p class="section-title"> Work experience </p>
+          <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px" @click="onAddWork()"><b-icon-plus font-scale="1.5" shift-v="-2"></b-icon-plus></button>
+          <div class="card mb-lg-1"  v-for="work in work_experience" :key="work.id">
+            <div class="card-header d-flex align-items-center">
+              <span class="card-title-work">{{work.job_name}}</span>
+              <button class="ml-auto btn btn-sm btn-danger" @click="deleteWork(work)">Delete</button>
+            </div>
+            <div class="card-body">
+              <p class="card-subtitle">{{work.company}}</p>
+              <p class="card-text">{{work.description}}</p>
+              <p class="card-text" v-if="!work.currently"><small class="text-muted">{{work.start_date}} - {{work.end_date}}</small></p>
+              <p class="card-text" v-if="work.currently"><small class="text-muted">{{work.start_date}} - now</small></p>
             </div>
           </div>
-          <div class="card-body">
-            <h5 class="card-title">{{work.company}}</h5>
-            <p class="card-text">{{work.description}}</p>
-            <p class="card-text" v-if="!work.currently"><small class="text-muted">{{work.start_date}} - {{work.end_date}}</small></p>
-            <p class="card-text" v-if="work.currently"><small class="text-muted">{{work.start_date}} - now</small></p>
-          </div>
         </div>
-      </div>
 
-      <div class="p-3 text-left">
-        <h3 style="font-family: 'Vollkorn'; display:inline-block"> Education </h3>
-        <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px" @click="onAddEducation()">Add education</button>
-        <div class="card mb-3" style="max-width: 700px"  v-for="ed in education" :key="ed.id">
-          <div class="card-body">
-            <div class="row align-items-center">
-              <div class="col">
-                <h5 class="card-title">{{ed.title}}</h5>
+        <div class="text-left p-2 pb-3" style="max-width: 50rem">
+          <p class="section-title"> Education </p>
+          <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px" @click="onAddEducation()"><b-icon-plus font-scale="1.5" shift-v="-2"></b-icon-plus></button>
+          <div class="card mb-lg-1" v-for="ed in education" :key="ed.id">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <span class="card-title-ed">{{ed.title}}</span>
+                <button class="ml-auto btn btn-sm btn-danger" @click="deleteEducation(ed)">Delete</button>
               </div>
-              <div class="d-flex flex-row-reverse">
-                <button class="btn btn-sm btn-danger m-1" @click="deleteEducation(ed)">Delete</button>
-                <!-- <button class="btn btn-sm m-1">Edit</button> -->
-              </div>
+              <p class="card-subtitle">{{ed.institution}}</p>
+              <p class="card-text" v-if="!ed.currently"><small class="text-muted">{{ed.start_date}} - {{ed.end_date}}</small></p>
+              <p class="card-text" v-if="ed.currently"><small class="text-muted">{{ed.start_date}} - now</small></p>
             </div>
-            <h6 class="card-title">{{ed.institution}}</h6>
-            <p class="card-text" v-if="!ed.currently"><small class="text-muted">{{ed.start_date}} - {{ed.end_date}}</small></p>
-            <p class="card-text" v-if="ed.currently"><small class="text-muted">{{ed.start_date}} - now</small></p>
+          </div>
+        </div>
+
+        <div class="text-left p-2 pb-3" style="max-width: 50rem">
+          <p class="section-title"> Skills </p>
+          <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px"><b-icon-plus font-scale="1.5" shift-v="-2"></b-icon-plus></button>
+          <div>
+            <span class="badge badge-pill badge-warning" style="font-size: 15px">Python</span>
+            <span class="badge badge-pill badge-warning" style="font-size: 15px">Java</span>
+            <span class="badge badge-pill badge-warning" style="font-size: 15px">SQL</span>
           </div>
         </div>
       </div>
 
-      <div class="p-3 text-left">
-        <h3 style="font-family: 'Vollkorn'; display:inline-block"> Skills </h3>
-        <button class="btn btn-sm" style="margin-bottom: 5px; margin-left: 20px">Add skills</button>
-        <div>
-          <span class="badge badge-pill badge-warning" style="font-size: 15px">Python</span>
-          <span class="badge badge-pill badge-warning" style="font-size: 15px">Java</span>
-          <span class="badge badge-pill badge-warning" style="font-size: 15px">SQL</span>
-        </div>
-      </div>
-    </div>
+      <b-modal hide-footer="true" hide-backdrop="false"  ref="addWorkModal">
+        <template #modal-header><h5 style="font-family: 'Work Sans SemiBold'">Add work experience</h5></template>
+        <b-form ref="addWorkForm" @submit.prevent="submitAddWork" @reset.prevent="resetAddWork" style="font-family: 'Work Sans SemiBold'">
+          <div class="form-group">
+            <label for="jobTitle">Job Title </label>
+            <input type="text" class="form-control" id="jobTitle" placeholder="Enter job title" v-model="addWork.jobName">
+          </div>
+          <div class="form-group">
+            <label for="companyName">Company </label>
+            <input type="text" class="form-control" id="companyName" placeholder="Enter company name" v-model="addWork.company">
+          </div>
+          <div class="form-group">
+            <label>Description </label>
+            <textarea class="form-control" id="description" rows="3" placeholder="Enter job description"
+                      v-model="addWork.description"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="startYear">Start month </label>
+            <input type="month" class="form-control" id="startYear" v-model="addWork.startDate">
+          </div>
+          <div class="form-group">
+            <label for="endYear">End month </label>
+            <input type="month" class="form-control" id="endYear" v-model="addWork.endDate">
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="currentlyCheckbox" v-model="addWork.currently">
+            <label class="form-check-label" for="currentlyCheckbox">Currently in this job</label>
+          </div>
+          <div class="float-right">
+            <b-button variant="primary" type="submit">Submit</b-button>
+            <b-button variant="secondary" type="reset">Reset</b-button>
+          </div>
+        </b-form>
+      </b-modal>
 
-    <b-modal hide-footer="true" hide-backdrop="false"  ref="addWorkModal">
-      <template #modal-header><h5 style="font-family: 'Work Sans SemiBold'">Add work experience</h5></template>
-      <b-form ref="addWorkForm" @submit.prevent="submitAddWork" @reset.prevent="resetAddWork" style="font-family: 'Work Sans SemiBold'">
-        <div class="form-group">
-          <label for="jobTitle">Job Title </label>
-          <input type="text" class="form-control" id="jobTitle" placeholder="Enter job title" v-model="addWork.jobName">
-        </div>
-        <div class="form-group">
-          <label for="companyName">Company </label>
-          <input type="text" class="form-control" id="companyName" placeholder="Enter company name" v-model="addWork.company">
-        </div>
-        <div class="form-group">
-          <label>Description </label>
-          <textarea class="form-control" id="description" rows="3" placeholder="Enter job description"
-                    v-model="addWork.description"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="startYear">Start month </label>
-          <input type="month" class="form-control" id="startYear" v-model="addWork.startDate">
-        </div>
-        <div class="form-group">
-          <label for="endYear">End month </label>
-          <input type="month" class="form-control" id="endYear" v-model="addWork.endDate">
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="currentlyCheckbox" v-model="addWork.currently">
-          <label class="form-check-label" for="currentlyCheckbox">Currently in this job</label>
-        </div>
-        <div class="float-right">
-          <b-button variant="primary" type="submit">Submit</b-button>
-          <b-button variant="secondary" type="reset">Reset</b-button>
-        </div>
-      </b-form>
-    </b-modal>
+      <b-modal hide-footer="true" hide-backdrop="false"  ref="addEducationModal">
+        <template #modal-header><h5 style="font-family: 'Work Sans SemiBold'">Add previous education</h5></template>
+        <b-form ref="addEducationForm" @submit.prevent="submitAddEducation" @reset.prevent="resetAddEducation" style="font-family: 'Work Sans SemiBold'">
+          <div class="form-group">
+            <label for="title">Title </label>
+            <input type="text" class="form-control" id="title" placeholder="Enter job title" v-model="addEducation.title">
+          </div>
+          <div class="form-group">
+            <label for="institution">Institution </label>
+            <input type="text" class="form-control" id="institution" placeholder="Enter institution" v-model="addEducation.institution">
+          </div>
+          <div class="form-group">
+            <label for="startYearEd">Start month </label>
+            <input type="month" class="form-control" id="startYearEd" v-model="addEducation.startDate">
+          </div>
+          <div class="form-group">
+            <label for="endYearEd">End month </label>
+            <input type="month" class="form-control" id="endYearEd" v-model="addEducation.endDate">
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="currentlyCheckboxEd" v-model="addEducation.currently">
+            <label class="form-check-label" for="currentlyCheckboxEd">Currently enrolled</label>
+          </div>
+          <div class="float-right">
+            <b-button variant="primary" type="submit">Submit</b-button>
+            <b-button variant="secondary" type="reset">Reset</b-button>
+          </div>
+        </b-form>
+      </b-modal>
 
-    <b-modal hide-footer="true" hide-backdrop="false"  ref="addEducationModal">
-      <template #modal-header><h5 style="font-family: 'Work Sans SemiBold'">Add previous education</h5></template>
-      <b-form ref="addEducationForm" @submit.prevent="submitAddEducation" @reset.prevent="resetAddEducation" style="font-family: 'Work Sans SemiBold'">
-        <div class="form-group">
-          <label for="title">Title </label>
-          <input type="text" class="form-control" id="title" placeholder="Enter job title" v-model="addEducation.title">
-        </div>
-        <div class="form-group">
-          <label for="institution">Institution </label>
-          <input type="text" class="form-control" id="institution" placeholder="Enter institution" v-model="addEducation.institution">
-        </div>
-        <div class="form-group">
-          <label for="startYearEd">Start month </label>
-          <input type="month" class="form-control" id="startYearEd" v-model="addEducation.startDate">
-        </div>
-        <div class="form-group">
-          <label for="endYearEd">End month </label>
-          <input type="month" class="form-control" id="endYearEd" v-model="addEducation.endDate">
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="currentlyCheckboxEd" v-model="addEducation.currently">
-          <label class="form-check-label" for="currentlyCheckboxEd">Currently enrolled</label>
-        </div>
-        <div class="float-right">
-          <b-button variant="primary" type="submit">Submit</b-button>
-          <b-button variant="secondary" type="reset">Reset</b-button>
-        </div>
-      </b-form>
-    </b-modal>
+    </b-container>
   </div>
 </template>
 
@@ -161,10 +157,13 @@ export default {
     return {
       name: 'Name Surname',
       username: 'sergi',
-      logged: true,
+      logged: false,
       work_experience: [],
       education: [],
       skills: ['Python', 'Java', 'SQL'],
+      bio: 'I’m living the dream. I’ve always been a great problem solver, an independent introvert, and a technophile obsessed with the latest devices.\n' +
+        'Today, I’m working from home as a software engineer for Google, and I get to show off all these elements of who I am.\n' +
+        ' I’m also eager to meet other software engineers in the area, so feel free to connect!',
       addWork: {
         jobName: '',
         company: '',
@@ -331,9 +330,38 @@ export default {
 
 <style >
 .navbar.navbar-light{
-  font-family: "Work Sans SemiBold";
+  font-family: "Work Sans SemiBold", sans-serif;
   font-size: 18px;
   padding: 20px;
   margin-bottom: 20px;
+}
+.bio-text {
+  text-align: left;
+  color: dimgray;
+  font-size: 16px;
+  padding-bottom: 20px;
+}
+.card-title-work {
+  font-family: "Work Sans SemiBold", Montserrat, sans-serif;
+  font-size: 20px;
+}
+.card-title-ed {
+  font-family: "Work Sans SemiBold", Montserrat, sans-serif;
+  font-size: 20px;
+  padding-bottom: 10px;
+}
+.card-subtitle {
+  font-family: "Work Sans SemiBold", Montserrat, sans-serif;
+  font-size: 18px;
+  padding-bottom: 5px;
+}
+.card-text {
+  font-family: "Work Sans", sans-serif;
+  font-size: 14px;
+}
+.section-title {
+  font-family: "Vollkorn",serif;
+  font-size: 26px;
+  display:inline-block
 }
 </style>
--- vue-gripau/src/main.js
+++ vue-gripau/src/main.js
@@ -1,10 +1,11 @@
-import BootstrapVue from 'bootstrap-vue'
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 import '@/../bootstrap/css/bootstrap.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({

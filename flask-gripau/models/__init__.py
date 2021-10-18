19be3978ba482750a153c80cc81738209827fe2f
alendinez <55415649+alendinez@users.noreply.github.com>
2021-10-18 22:32:14 +0200

    added /api/ before api endpoints

--- flask-gripau/models/__init__.py
+++ flask-gripau/models/__init__.py
@@ -2,3 +2,4 @@ from models.job_seeker import JobSeekersModel
 from models.company import CompanyModel
 from models.education import EducationsModel
 from models.work_experience import WorkExperiencesModel
+from models.job_offer import JobOfferModel

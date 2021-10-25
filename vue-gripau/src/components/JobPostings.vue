85d3ccbd661e419a3d629216950e92a6e40ba950
David Rial Figols <davidrialfigols@gmail.com>
2021-10-25 20:58:53 +0200

    init job offer form

--- vue-gripau/src/components/JobPostings.vue
+++ vue-gripau/src/components/JobPostings.vue
@@ -29,6 +29,16 @@
 
     <h2 style="font-family: 'Vollkorn"> {{ message }} </h2>
     <b-container fluid>
+      <b-row align-h="center" v-if="is_company">
+        <b-card
+          tag="article"
+          class="text-center"
+          style="width: 90%; max-width: 600px"
+        >
+          <b-link v-b-modal.job-offer-modal style="position: absolute; top:0; left:0; height: 100%; width:100%"></b-link>
+          <p class="h1 mb-2"><b-icon icon="patch-plus"></b-icon></p>
+        </b-card>
+      </b-row>
       <b-row align-h="center" v-for="(job_offer) in job_offers" :key="job_offer.id">
         <b-card
           :title="job_offer.jobName"
@@ -38,37 +48,133 @@
           align="left"
         >
           <b-card-text>
-            {{ job_offer.description }}
+            {{ job_offer.company }}
           </b-card-text>
           <footer>
-            <b-icon icon="alarm"></b-icon> {{ job_offer.type}}
-            <b-icon icon="building"></b-icon> {{ job_offer.company}}
+            <b-icon icon="briefcase"></b-icon> {{ job_offer.contract_type }}
+            <b-icon icon="alarm"></b-icon> {{ job_offer.working_hours }}
+            <b-icon icon="calendar3-event"></b-icon> {{ job_offer.publication_date }}
+            <b-icon icon="geo-alt-fill"></b-icon> {{ job_offer.location }}
           </footer>
         </b-card>
       </b-row>
     </b-container>
+    <b-modal ref="jobOfferModal"
+             id="job-offer-modal"
+             title="Post a job offer"
+             hide-footer
+             hide-backdrop
+    >
+      <validation-observer ref="observer" v-slot="{ handleSubmit }">
+        <b-form style="font-family:'Work Sans'" @submit.prevent="handleSubmit(onSubmit)">
+
+          <validation-provider name="JobName"  :rules="{ alpha_spaces, required: true, max: 40}" v-slot="validationContext">
+            <b-form-group id="input-group-1" label="Job name" label-for="input-1">
+              <b-form-input v-model="jobOfferForm.jobName" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-1-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-1-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="Description"  :rules="{ max: 500}" v-slot="validationContext">
+            <b-form-group id="input-group-2" label="Description" label-for="input-2">
+              <b-form-input v-model="jobOfferForm.description" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-2-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-2-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="Salary"  :rules="{ numeric, max: 10}" v-slot="validationContext">
+            <b-form-group id="input-group-3" label="Salary" label-for="input-3">
+              <b-form-input v-model="jobOfferForm.salary" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-3-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-3-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="VacancyNumber"  :rules="{ numeric, max: 3}" v-slot="validationContext">
+            <b-form-group id="input-group-4" label="Vacancy number" label-for="input-4">
+              <b-form-input v-model="jobOfferForm.vacancyNumber" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-4-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-4-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="Location"  :rules="{ alpha_spaces, required: true, max: 40}" v-slot="validationContext">
+            <b-form-group id="input-group-1" label="Location" label-for="input-1">
+              <b-form-input v-model="jobOfferForm.location" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-1-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-1-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="ContractType"  :rules="{ max: 500}" v-slot="validationContext">
+            <b-form-group id="input-group-2" label="Contract type" label-for="input-2">
+              <b-form-input v-model="jobOfferForm.contractType" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-2-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-2-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="WorkingHours"  :rules="{ numeric, max: 10}" v-slot="validationContext">
+            <b-form-group id="input-group-3" label="Working hours" label-for="input-3">
+              <b-form-input v-model="jobOfferForm.workingHours" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-3-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-3-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <validation-provider name="MinimumExperience"  :rules="{required: true, max: 300}" v-slot="validationContext">
+            <b-form-group id="input-group-4" label="Minimum experience" label-for="input-4">
+              <b-form-input v-model="jobOfferForm.minimumExperience" placeholder="" type="text" :state="getValidationState(validationContext)"
+                            aria-describedby="input-4-live-feedback"></b-form-input>
+              <b-form-invalid-feedback id="input-4-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-warning justify-content-md-end">Post Job Offer!</button>
+          </div>
+        </b-form>
+      </validation-observer>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import Vue from 'vue'
+import axios from 'axios'
 
 export default {
   data () {
     return {
       message: 'Job Postings',
       logged: false,
-      job_offers: [{'id': 0, 'jobName': 'Waiter', 'company': 'Terra Restaurant', 'description': 'This offer is fake and it will be deleted in the next update', 'type': 'Part time'}, {'id': 1, 'jobName': 'Journalist', 'company': 'The Sun', 'description': 'This offer is fake and it will be deleted in the next update', 'type': 'Part time'}],
+      job_offers: [], // {'id': 0, 'jobName': 'Waiter', 'company': 'Terra Restaurant', 'description': 'This offer is fake and it will be deleted in the next update', 'type': 'Part time'}, {'id': 1, 'jobName': 'Journalist', 'company': 'The Sun', 'description': 'This offer is fake and it will be deleted in the next update', 'type': 'Part time'}],
       username: '',
       token: '',
       is_jobseeker: true,
       is_company: false,
-      is_admin: false
+      is_admin: false,
+      jobOfferForm: {
+        jobName: '',
+        description: '',
+        salary: '',
+        vacancyNumber: '',
+        location: '',
+        contractType: '',
+        workingHours: '',
+        minimumExperience: ''
+      }
     }
   },
   methods: {
     onHome () {
       this.$router.replace({ path: '/' })
+    },
+    getValidationState ({ dirty, validated, valid = null }) {
+      return dirty || validated ? valid : null
     },
     onUserProfile () {
       if (this.is_jobseeker & this.logged) {
@@ -87,6 +193,52 @@ export default {
     },
     onAboutUs () {
       this.$router.replace({ path: '/about_us' })
+    },
+    getJobOffers () {
+      const path = Vue.prototype.$API_BASE_URL + 'offers'
+      axios.get(path)
+        .then((res) => {
+          this.offers = res.data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    initJobOfferForm () {
+      this.jobOfferForm.jobName = ''
+      this.jobOfferForm.description = ''
+      this.jobOfferForm.salary = ''
+      this.jobOfferForm.vacancyNumber = ''
+      this.jobOfferForm.location = ''
+      this.jobOfferForm.contractType = ''
+      this.jobOfferForm.workingHours = ''
+      this.jobOfferForm.minimumExperience = ''
+    },
+    onSubmit () {
+      const path = Vue.prototype.$API_BASE_URL + 'job_offer/' + this.username
+      const values = {
+        jobName: this.jobOfferForm.jobName,
+        description: this.jobOfferForm.description,
+        salary: this.jobOfferForm.salary,
+        vacancy_number: this.jobOfferForm.vacancyNumber,
+        location: this.jobOfferForm.location,
+        contract_type: this.jobOfferForm.contractType,
+        working_hours: this.jobOfferForm.workingHours,
+        minimum_experience: this.jobOfferForm.minimumExperience,
+        publication_date: '10/10/2021'
+      }
+      axios.post(path, values)
+        .then((res) => {
+          console.log('Job Offer correctly posted')
+        })
+        .catch((error) => {
+          alert(error.response.data.message)
+        })
+      this.onReset()
+      this.$bvModal.hide('job-offer-modal')
+    },
+    onReset () {
+      this.initJobOfferForm()
     }
   },
   created () {
@@ -96,6 +248,7 @@ export default {
     this.is_company = this.$store.state.isCompany
     this.token = this.$store.state.token
     this.is_admin = this.$store.state.isAdmin
+    this.getJobOffers()
   },
   computed: mapState({
     token: state => state.token,

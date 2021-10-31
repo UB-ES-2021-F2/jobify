3f30ce70b46b1500571b665060b3a45257d87ee0
David Rial Figols <davidrialfigols@gmail.com>
2021-10-31 13:22:28 +0100

    CompanyJobOffer api testing

--- vue-gripau/cypress/integration/1-resources/CompanyJobOffer.spec.js
+++ vue-gripau/cypress/integration/1-resources/CompanyJobOffer.spec.js
@@ -5,17 +5,29 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-describe('Companies resource', () => {
-  context('GET /api/company/username', () => {
-    it('should return the information of the company UB', () => {
+describe('CompanyJobOffer resource', () => {
+  context('GET /api/offers/companyname', () => {
+    it('should return the information of all job offers posted by company UB (now only one job offer)', () => {
       cy.request({
         method: 'GET',
-        url: 'http://localhost:5000/api/company/ub'
+        url: 'http://localhost:5000/api/offers/ub'
       })
         .should((response) => {
           cy.log(JSON.stringify(response.body))
           expect(response.status).to.eq(200)
-          expect(response.body.account.company).to.eq('ub')
+          expect(response.body[0].company).to.eq('ub')
+          expect(response.body[0].job_name).to.eq('professor')
+          expect(response.body.length).to.eq(1)
+        })
+    })
+    it('should return error because the company "ub2" can not exist', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:5000/api/offers/ub2',
+        failOnStatusCode: false
+      })
+        .should((response) => {
+          expect(response.status).to.eq(404)
         })
     })
   })

6140631cbcc03a98ebdb90bd8780394a83f84961
Sergi Ger <sergiger00@gmail.com>
2021-11-22 23:06:53 +0100

    jest configured

--- vue-gripau/tests/unit/home.spec.js
+++ vue-gripau/tests/unit/home.spec.js
@@ -12,10 +12,10 @@ describe('Homepage not logged ', () => {
           state: {
             logged : false,
             username : null,
-            is_jobseeker : false,
-            is_company : false,
+            isJobSeeker : false,
+            isCompany : false,
             token : null,
-            is_admin : false,
+            isAdmin : false,
             name : ""
           }
         }
@@ -29,7 +29,7 @@ describe('Homepage not logged ', () => {
   it('logged', () => {
     expect(wrapper.vm.logged).toEqual(false)
   })
-  it('isJobseeker', () => {
+  it('is_jobseeker', () => {
     expect(wrapper.vm.is_jobseeker).toEqual(false)
   })
   it('is_company', () => {
@@ -41,32 +41,4 @@ describe('Homepage not logged ', () => {
   it('is_admin', () => {
     expect(wrapper.vm.is_admin).toEqual(false)
   })
-  it('name', () => {
-    expect(wrapper.vm.name).toEqual("")
-  })
-
-  /*it('renders a value from $store.state', () => {
-    console.log(wrapper.vm.welcome_message)
-    expect(wrapper.find('logged').name().trim()).toEqual('false')
-  })*/
-  /*it('renders a $store.state value return from computed', () => {
-    const wrapper = shallow(State, {
-      computed: {
-        value_2: () => 'value_2'
-      },
-      localVue
-    })
-    expect(wrapper.find('.state-2')
-      .text().trim()).toEqual('value_2')
-  })
-  it('renders a $store.state value return from mapState', () => {
-    const wrapper = shallow(State, {
-      computed: {
-        value_3: () => 'value_3'
-      },
-      localVue
-    })
-    expect(wrapper.find('.state-3')
-      .text().trim()).toEqual('value_3')
-  })*/
 })

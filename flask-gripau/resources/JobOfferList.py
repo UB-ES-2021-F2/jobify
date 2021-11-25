e00c1192da93ffd524a97b055ec23104934f340d
David Rial Figols <davidrialfigols@gmail.com>
2021-11-25 22:54:19 +0100

    added keyword filter

--- flask-gripau/resources/JobOfferList.py
+++ flask-gripau/resources/JobOfferList.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from models import JobOfferModel
 from db import db
 from models.company import CompanyModel
+from fuzzywuzzy import fuzz
 
 class JobOfferList(Resource):
     """Resource that lists all the job offers in the database"""
@@ -15,8 +16,34 @@ class JobOfferList(Resource):
         Returns:
 
         """
+
+        parser = reqparse.RequestParser()  # create parameters parser from request
+        parser.add_argument('keyword', type=str, required=False)
+
+        data = parser.parse_args()
+
+
         offers = []
-        for company in CompanyModel.query.all():
-            for offer in company.job_offers:
-                offers.append(offer.json())
+        if data.keyword is None:
+            for company in CompanyModel.query.all():
+                for offer in company.job_offers:
+                    offers.append(offer.json())
+        else:
+            for company in CompanyModel.query.all():
+                to_append = False
+                for word in company.company.split(" "):
+                    if fuzz.ratio(word, data.keyword) > 80:
+                        to_append = True
+                        break
+                for offer in company.job_offers:
+                    if to_append:
+                        offers.append(offer.json())
+                    else:
+                        offer_name = offer.job_name
+                        appended=False
+                        for word in offer_name.split(" "):
+                            if fuzz.ratio(word, data.keyword) > 80:
+                                offers.append(offer.json())
+                                appended = True
+                                break
         return {'OfferList': offers}, 200

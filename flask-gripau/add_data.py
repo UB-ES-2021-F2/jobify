1705de72de55c787191e63694fa4ee270adcdd09
Carles Perez Onielfa <98perezcarles@gmail.com>
2021-11-28 14:50:00 +0100

    Arreglat add_data perque funcioni a heroku

--- flask-gripau/add_data.py
+++ flask-gripau/add_data.py
@@ -2,7 +2,8 @@ import datetime
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from decouple import config as config_decouple
+from config import config
 # import models here
 from models import EducationsModel, WorkExperiencesModel, JobOfferModel, SkillsModel
 from models.job_seeker import JobSeekersModel
@@ -11,8 +12,10 @@ from models.education import EducationsModel
 from models.job_offer import JobOfferModel
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+environment = config['development']
+if config_decouple('PRODUCTION', cast=bool, default=False):
+    environment = config['production']
+app.config.from_object(environment)
 db = SQLAlchemy(app)
 db.init_app(app)
 
